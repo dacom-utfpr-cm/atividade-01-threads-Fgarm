f58066fbe89b49a6bb58d6c5d7c021f9f6ea0d8d
Fgarm <guilhermear.maturana@outlook.com>
2024-10-23 17:04:32 -0300

    Implements Ex 04

--- atvd04/Main.java
+++ atvd04/Main.java
@@ -1,13 +1,35 @@
 package atvd04;
 
+import atvd02.ReadRandomInterval;
+import java.io.FileNotFoundException;
+
 public class Main {
     public static void main(String[] args) {
-        ThreadMonitora monitor = new ThreadMonitora();
-
-        // Add a thread through the monitor
-        // monitor.addThread(new Thread(() -> System.out.println("Running...")));
-
-        // Check if the original list was modified
-        // System.out.println("Number of threads: " + threads.size()); // Should print 1
+        try {
+            ThreadMonitora monitor = new ThreadMonitora();
+            // TODO: Trocar sleep por alguma espera de tempo ocupada, por que thread parada não pode ser interrompida
+            Thread Leitor1 = new Thread(new ReadRandomInterval(0));
+            Thread Leitor2 = new Thread(new ReadRandomInterval(0));
+            Thread Leitor3 = new Thread(new ReadRandomInterval(0));
+            monitor.addThread(Leitor1);
+            monitor.addThread(Leitor2);
+            monitor.addThread(Leitor3);
+            Thread Threadmonitora = new Thread(monitor);
+            Threadmonitora.start();
+            Leitor1.start();
+            Leitor1.interrupt();
+            Leitor2.start();
+            Leitor2.interrupt();
+            Leitor3.start();
+            Leitor3.interrupt();
+            //Thread.sleep(4000);
+            //Thread.sleep(8000);
+            Threadmonitora.join();
+        } catch (FileNotFoundException e) {
+            
+            System.out.println("Arquivo da Thread leitora não encontrado");
+        } catch (InterruptedException e) {
+            System.out.println("A main foi interrompida");
+        }
     }
 }
--- atvd04/ThreadMonitora.java
+++ atvd04/ThreadMonitora.java
@@ -1,23 +1,56 @@
 package atvd04;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-public class ThreadMonitora {
-    private final List<Thread> myList;
+public class ThreadMonitora implements Runnable {
+    private final List<Thread> ThreadList;
+    private final Map<Thread, Boolean> InterruptedMap;
+    private int contador = 0;
 
     public ThreadMonitora() {
-        this.myList = new ArrayList<>();
+        this.ThreadList = new ArrayList<>();
+        this.InterruptedMap = new HashMap<>();
     }
 
     public void addThread(Thread thread) {
-        this.myList.add(thread);
+        this.ThreadList.add(thread);
+        this.InterruptedMap.put(thread, false);
+        this.contador += 1;
     }
 
     public ThreadMonitora(List<Thread> lista) {
         if (lista == null) {
             throw new IllegalArgumentException("The list cannot be null.");
         }
-        this.myList = lista;
+        this.ThreadList = lista;
+        this.InterruptedMap = new HashMap<>();
     }
+
+    @Override
+    public void run() {
+        try {
+        int interrompidas = 0;
+        while(interrompidas < this.contador){
+            Thread.sleep(100);
+            interrompidas = 0;
+            for (Thread tredi : this.ThreadList) {
+                
+                if(tredi.isInterrupted()) {
+                    if(!this.InterruptedMap.get(tredi)) {
+                        System.out.println("Thread " + tredi + " foi interrompida.");
+                        this.InterruptedMap.put(tredi, true);
+                    }
+                    interrompidas += 1;
+                }
+            }
+        }
+        System.out.println("Todas as threads foram interrompidas. Monitor será parado");
+        } catch (InterruptedException ex) {
+            System.out.println("Quem vigia o vigia?");
+        }
+    }
+
 }
