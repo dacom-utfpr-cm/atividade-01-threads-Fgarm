6a23adaef24f58643f63e9f0234f7b9a3388b95e
Fgarm <guilhermear.maturana@outlook.com>
2024-10-24 16:19:33 -0300

    Finish exercises

--- atvd05/Main.java
+++ atvd05/Main.java
@@ -1,5 +1,19 @@
 package atvd05;
 
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        ThreadSomadora somador = new ThreadSomadora();
+        Thread ThreadSum = new Thread(somador);
+        ThreadSum.start();
+        ThreadSum.join();
+        List<Float> vetor = somador.getVetor();
+        Double soma = 0.0;
+        for (Float elemento : vetor) {
+            soma += elemento;
+        }
+        System.out.println("Soma total: " + soma + ".");
+    }
 
 }
--- atvd05/ThreadSomadora.java
+++ atvd05/ThreadSomadora.java
@@ -1,9 +1,10 @@
 package atvd05;
 
-import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ThreadSomadora implements Runnable {
-    private final List<Integer> VetorSoma;
+    private final ArrayList<Float> VetorSoma;
 
     
 
@@ -11,9 +12,28 @@ public class ThreadSomadora implements Runnable {
         this.VetorSoma = new ArrayList<>();
     }
 
+    public ThreadSomadora(ArrayList<Float> array) {
+        this.VetorSoma = array;
+    }
+
+    public ArrayList<Float> getVetor() {
+        return this.VetorSoma;
+    }
+
+    private void addElementos(float elemento) {
+        this.VetorSoma.add(elemento);
+    }
     @Override
     public void run() {
+        System.out.println("Insira números para serem somados, 0 para sair");
 
+        try (Scanner scanner = new Scanner(System.in)) {
+            float elemento;
+            do { 
+                elemento = scanner.nextFloat();
+                this.addElementos(elemento);
+            } while (elemento != 0);
+        }
     }
 
 }
